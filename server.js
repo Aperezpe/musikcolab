1ea54f3a2de2ed7d72e16c86ba8164e1b0f2dbe9
armaglz <arma_glz@hotmail.com>
2019-12-03 21:48:56 -0600

    temp home page

--- server.js
+++ server.js
@@ -1,109 +1,95 @@
 // import the models
 // const { User } = require('./models');
-const { User, Album, Song, Song_likes } = require('./models');
+const { User, Album, Song, Song_likes } = require("./models");
 
-const express = require('express');
-const path = require('path');
-var hbs = require('express-handlebars');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const path = require("path");
+var hbs = require("express-handlebars");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 
 app = express();
-app.set('port', 3002);
+app.set("port", 3002);
 
 // setup handlebars and the view engine for res.render calls
 // (more standard to use an extension like 'hbs' rather than
 //  'html', but the Universiry server doesn't like other extensions)
-app.set('view engine', 'html');
-app.engine('html', hbs({
-  extname: 'html',
-  defaultView: 'default',
-  layoutsDir: __dirname + '/views/layouts/',
-  partialsDir: __dirname + '/views/partials/'
-}));
+app.set("view engine", "html");
+app.engine(
+  "html",
+  hbs({
+    extname: "html",
+    defaultView: "default",
+    layoutsDir: __dirname + "/views/layouts/",
+    partialsDir: __dirname + "/views/partials/"
+  })
+);
 
 // setup body parsing for form data
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // set up session (in-memory storage by default)
 app.use(session({ secret: "This is a big long secret lama string." }));
 // setup static file service
-app.use(express.static(path.join(__dirname, 'static')));
-
-
+app.use(express.static(path.join(__dirname, "static")));
 
 //Log in get and post
-app.get('/login', function (req, res, next) {
+app.get("/login", function(req, res, next) {
   if (req.session.user) {
     if (req.session.user.admin == 0 || req.session.user.admin == 1) {
-      res.redirect("/")
+      res.redirect("/");
     }
-  }
-  else
-    res.render("login");
-
+  } else res.render("login");
 });
 
-app.post('/login', function (req, res, next) {
-
-  let errors = []
+app.post("/login", function(req, res, next) {
+  let errors = [];
   if (req.body.username.length == 0 || req.body.pass.length == 0) {
-    errors.push({ msg: "Uername or Password Incorect!!!" })
+    errors.push({ msg: "Uername or Password Incorect!!!" });
     res.render("login", { errors });
-  }
-  else {
+  } else {
     User.findOne({ where: { username: req.body.username } }).then(user => {
       if (user == null) {
-        errors.push({ msg: "Username or Password Incorect!!!" })
+        errors.push({ msg: "Username or Password Incorect!!!" });
         res.render("login", { errors });
-      }
-      else {
+      } else {
         bcrypt.compare(req.body.pass, user.has_password).then(result => {
-
           if (result) {
             req.session.user = user;
             res.redirect("/");
-          }
-
-          else {
-            errors.push({ msg: "Username or Password Incorect!!!" })
+          } else {
+            errors.push({ msg: "Username or Password Incorect!!!" });
             res.render("login", { errors });
           }
-        })
+        });
       }
     });
   }
-
 });
 
-
-//Register get and post page 
-app.get('/register', function (req, res, next) {
+//Register get and post page
+app.get("/register", function(req, res, next) {
   if (req.session.user) {
     if (req.session.user.admin == 0 || req.session.user.admin == 1) {
-      res.redirect("/")
+      res.redirect("/");
     }
-  }
-  else
-    res.render("register");
-
+  } else res.render("register");
 });
 
-app.post('/register', function (req, res, next) {
-  let errors = []
+app.post("/register", function(req, res, next) {
+  let errors = [];
   if (req.body.username.length == 0) {
-    errors.push({ msg: "Username not provide" })
+    errors.push({ msg: "Username not provide" });
   }
   if (req.body.pass.length < 4) {
-    errors.push({ msg: "Password conot be less that 4 characters" })
+    errors.push({ msg: "Password conot be less that 4 characters" });
   }
   User.findOne({ where: { username: req.body.username } }).then(user => {
     n_user = user;
     if (n_user == null) {
       if (errors.length == 0) {
-
         User.create({
           username: req.body.username,
           artist_name: req.body.artistname,
@@ -113,67 +99,44 @@ app.post('/register', function (req, res, next) {
         });
       }
       res.redirect("/login");
-    }
-    else if (req.body.username == n_user.username) {
-
-      errors.push({ msg: "Username Not Avilable!!!" })
-
-    }
-    else {
+    } else if (req.body.username == n_user.username) {
+      errors.push({ msg: "Username Not Avilable!!!" });
+    } else {
       let us = req.body.username;
       res.render("register", { errors, us });
     }
-  })
+  });
   //res.render("register");
-
 });
 
-
 // Display all teh users just for testing
-app.get('/all_users', function (req, res, next) {
+app.get("/all_users", function(req, res, next) {
   if (req.session.user) {
     if (req.session.user.admin) {
       User.findAll().then(user => {
         res.render("test", { user: user });
       });
-    }
-    else
-      res.redirect("/login")
-  }
-  else
-    res.redirect("/login")
+    } else res.redirect("/login");
+  } else res.redirect("/login");
 });
-
 
 //test
-app.get('/', function (req, res, next) {
-  console.log("Hola" + req.session.user);
-  res.render("home")
+app.get("/", function(req, res, next) {
+  //console.log("Hola" + req.session.user);
+  res.render("home");
 });
 
-app.post('/where', function (req, res, next) {
-
-  if (req.body.b == "login")
-    res.redirect("/login");
-
-  else if (req.body.b == "users")
-    res.redirect("/all_users");
-
-  else if (req.body.b == "reg")
-    res.redirect("/register");
-
-
-
+app.post("/where", function(req, res, next) {
+  if (req.body.b == "login") res.redirect("/login");
+  else if (req.body.b == "users") res.redirect("/all_users");
+  else if (req.body.b == "reg") res.redirect("/register");
 });
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   // remove user from session
   delete req.session.user;
-  res.redirect('/');
+  res.redirect("/");
 });
 
-var server = app.listen(app.get('port'), function () {
-  console.log("Server started...")
+var server = app.listen(app.get("port"), function() {
+  console.log("Server started...");
 });
-
-
-
